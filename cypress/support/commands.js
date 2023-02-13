903948471c61b38c3ca0e21096297c07bf1fdb4f
Mike Weaver <michael@triplewhale.com>
2023-02-13 14:04:29 -0500

    WIP demo

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -35,30 +35,67 @@ Cypress.Commands.add('madisonPod', () => {
 
 const urlPrefix = 'https://api.triplewhale.com/api/v2/'
 Cypress.Commands.add('stubSummaryResponses', () => {
-  cy.intercept('POST', `${urlPrefix}/summary-page/compare-stats*`, {
-    statusCode: 200,
-    fixture: 'compare-stats.json'
-  }).as('compare-stats')
+  // STATS
+  cy.fixture('compare-stats.json').then((fixture) => {
+    cy.intercept('POST', `${urlPrefix}/summary-page/compare-stats*`, (req) => {
+      // fixture.mainDatePickerSelectionRange.start = req.periods[0].start
+      // fixture.mainDatePickerSelectionRange.end = req.periods[0].end
+      // fixture.stats[0].start = req.periods[0].start
+      // fixture.stats[0].end = req.periods[0].end
+      
+      return {
+        statusCode: 200,
+        fixture: fixture
+      }
+    }).as('compare-stats')
+  })
 
   cy.intercept('POST', `${urlPrefix}/attribution/get-all-stats*`, {
     statusCode: 200,
     fixture: 'get-all-stats.json'
   }).as('get-all-stats')
 
+  cy.intercept('POST', `${urlPrefix}/attribution/get-order-stats-wrapper*`, {
+    statusCode: 200,
+    fixture: 'get-order-stats.json'
+  }).as('get-order-stats-wrapper')
+
+  // METRICS
   cy.intercept('POST', `${urlPrefix}/attribution/get-metrics*`, {
     statusCode: 200,
-    fixture: 'get-metrics.json'
+    fixture: 'metrics.json'
   }).as('get-metrics')
 
-  cy.intercept('GET', `${urlPrefix}/tw-metrics/metrics-data*`, {
+  cy.intercept('GET', `${urlPrefix}/**/metrics-data*`, {
     statusCode: 200,
-    fixture: 'metrics-data-get.json'
+    fixture: 'metrics.json'
   }).as('metrics-data')
 
-  cy.intercept('POST', `${urlPrefix}/metrics-table/metrics-data*`, {
+  cy.intercept('POST', `${urlPrefix}/**/metrics/*`, {
     statusCode: 200,
-    fixture: 'metrics-data-post.json'
+    fixture: 'metrics.json'
   }).as('metrics-data')
+
+  // FINANCE
+  cy.intercept('GET', `${urlPrefix}/finance/codat/pnl/all`, {
+    statusCode: 200,
+    fixture: 'finance-pnl.json'
+  }).as('finance-pnl')
+
+  cy.intercept('GET', `${urlPrefix}/finance/plaid/all-balance`, {
+    statusCode: 200,
+    fixture: 'finance-balance.json'
+  }).as('finance-balance')
+
+  cy.intercept('GET', `${urlPrefix}/finance/codat/categories/all`, {
+    statusCode: 200,
+    fixture: 'finance-categories.json'
+  }).as('finance-categories')
+
+  cy.intercept('GET', `${urlPrefix}/finance/codat/transactions/all`, {
+    statusCode: 200,
+    fixture: 'finance-transactions.json'
+  }).as('finance-transactions')
 
   // cy.intercept('GET', `${urlPrefix}/**/*`, { data: [] }).as('catch-all')
   // cy.intercept('POST', `${urlPrefix}/**/*`, { data: [] }).as('catch-all')
